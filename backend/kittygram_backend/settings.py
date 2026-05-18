0cc7db08599482b92e627d6382f263de10507851
Kirill Aristov <kirillaristov14@yandex.ru>
2026-05-18 21:11:35 +0300

    last_update_04

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 import os
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -10,6 +11,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    SECRET_KEY = get_random_secret_key()
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
